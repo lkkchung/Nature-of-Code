146596f2d66626f323846bf7a8e959b5fa41288f
Lucas <lucas@172-16-250-148.DYNAPOOL.NYU.EDU>
2018-04-03 14:23:24 -0400

    First sketch commit and adding style.css

---
+++ ML_CLass/sketch.js
@@ -0,0 +1,33 @@
+let brain;
+
+function setup() {
+  createCanvas(400, 400);
+  //let color = colorPredictor(255, 100, 50);
+
+  brain = new NeuralNetwork(3, 3, 2);
+
+  let inputs = [1, 0.9, 0.9];
+  let outputs = brain.predict(inputs);
+
+  console.log(outputs);
+
+  let targets = [0, 1];
+  brain.train(inputs, targets);
+  outputs = brain.predict(inputs);
+
+  console.log(outputs[0], outputs[1]);
+  //
+  // if (outputs[1] > outputs[0]) {
+  //   console.log('use white');
+  // } else {
+  //   console.log('use black');
+  // }
+
+
+}
+
+function colorPredictor(r, g, b) {
+  if (r + g + b > 300) {
+    return "black";
+  }
+}
